ee1a42fc3396abfb031bdeba3a038aeb421cdd8e
Marina982 <159331002+Marina982@users.noreply.github.com>
2024-11-25 14:35:38 -0300

    save

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,47 @@
 import { useState } from "react";
 
 export default function Registrar() {
+ 
+  const [Nome, setNome] = useState('');
+  const [Email, setEmail] = useState('');
+
+    const registrar = async (event) => {
+      event.preventDefault();
+try{
+    await fetch('http://localhost:3000/usuarios',{
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      nome: Nome,
+      email: Email
+    })
+  }
+  )
+}catch{
+  alert('Ocorreu um erro na aplicação')
+}
+}
 
 
   return (
-        
+        <main>
+          <form onSubmit={registrar}>
+
+<input 
+type="text" 
+value={Nome} 
+placeholder="Nome" 
+onChange={(event) => setNome(event.target.value)}/>
+
+<input 
+type="text"
+value={Email} 
+placeholder="Email" 
+onChange={(event) => setEmail(event.target.value)}/>
+
+<button type="submit">Registrar</button>
+
+          </form>
+        </main>
   );
 }
