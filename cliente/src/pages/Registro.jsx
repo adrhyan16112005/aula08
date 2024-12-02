53acbcc7b0a9d33d534567a55e9223dc54cc0c7a
Marina982 <159331002+Marina982@users.noreply.github.com>
2024-12-02 14:24:38 -0300

    save

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,24 +1,30 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom"
+
 
 export default function Registrar() {
  
   const [Nome, setNome] = useState('');
   const [Email, setEmail] = useState('');
 
+  const navigation = useNavigate() ;
+
     const registrar = async (event) => {
       event.preventDefault();
 try{
-    await fetch('http://localhost:3000/usuarios',{
+    const resposta = await fetch('http://localhost:3000/usuarios',{
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
       nome: Nome,
       email: Email
     })
+  });
+  if(resposta.ok){
+    navigation('/');
   }
-  )
-}catch{
-  alert('Ocorreu um erro na aplicação')
+}catch(err){
+  alert('Ocorreu um erro na aplicação', err);
 }
 }
 
