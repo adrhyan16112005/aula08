56d25adaae4c1685ecdabdda4e7c7215dee3bdcf
adrhyan16112005 <adrhyanviniciusnunesflores@gmail.com>
2025-01-28 09:04:07 -0300

    naveg

--- cliente/src/main.jsx
+++ cliente/src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -6,14 +5,6 @@ import Home from './pages/Home'
 import Registrar from './pages/Registro'
 import Alterar from './pages/Alterar'
 import Header from './components/Header';
-=======
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Registrar from './pages/Registrar';
-import Alterar from './pages/Alterar';  // Completar a importação aqui
->>>>>>> 9758ae60ff2192a9b2f46832a01f757d874abe27
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
